8cc0912594117eb4dcabf8b2c6c616e4c8b5be09
CValeria <124835561+CValeria@users.noreply.github.com>
2023-06-15 19:28:58 +0300

    Fixes

--- src/App.js
+++ src/App.js
@@ -269,7 +269,7 @@ function FactList({ facts, setFacts }) {
 
   return (
     <section>
-      <ul className="facts=list">
+      <ul className="facts-list">
         {facts.map((fact) => (
           <Fact key={fact.id} fact={fact} setFacts={setFacts} />
         ))}
